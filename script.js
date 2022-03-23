0fe0d20bbedbeb07eb73cb013c22ad2e3d164e6f
Muhammad Mohsin <mohsinraja4667@gmail.com>
2022-03-23 18:49:41 +0530

    mychange

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('hello World')
+let resBtn = document.querySelector('.resBtn');
+let showUl = document.querySelector('.showUL')
+
+resBtn.addEventListener('click', () => {
+    showUl.style.display = 'block';
+    setTimeout(() => {
+        showUl.style.display = 'none';
+    }, 10000)
+
+})
